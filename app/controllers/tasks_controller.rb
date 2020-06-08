8e335f4c7e250e043eb8704a1b8d7b9fa0b34cb5
Haruka Teduka <th_kiyose@outlook.com>
2020-06-08 11:59:20 +0900

    showの表示内容を変更

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,5 +1,14 @@
 class TasksController < ApplicationController
+  before_action :set_task, only: %i(show)
   def index
     @tasks = Task.all
+  end
+
+  def show
+  end
+
+  private
+  def set_task
+    @task = Task.find(params[:id])
   end
 end
