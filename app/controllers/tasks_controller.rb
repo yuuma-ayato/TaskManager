a963b0e243e1ec941b1cb1e8c92ff328c70fc5e2
Haruka Teduka <th_kiyose@outlook.com>
2020-06-08 14:03:04 +0900

    createアクション定義

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -8,11 +8,21 @@ class TasksController < ApplicationController
     @task = Task.new
   end
 
+  def create
+    @task = Task.new(get_task_params)
+    @task.save
+    redirect_to tasks_path
+  end
+
   def show
   end
 
   private
   def set_task
     @task = Task.find(params[:id])
+  end
+
+  def get_task_params
+    params.require(:task).permit(:content,:detail)
   end
 end
