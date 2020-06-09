421e7d9bddface293e7df1babef0f2b3e55bf4fa
Haruka Teduka <th_kiyose@outlook.com>
2020-06-09 17:07:33 +0900

    バリデーションテストのModel Specを記述

--- spec/models/task_spec.rb
+++ spec/models/task_spec.rb
@@ -1,5 +1,20 @@
 require 'rails_helper'
 
-RSpec.describe Task, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+RSpec.describe 'タスク管理機能', type: :model do
+  it 'contentが空ならバリデーションが通らない' do
+    task = Task.new(content:"", detail: "失敗テスト")
+    expect(task).not_to be_valid
+  end
+  it 'contentが50文字以上ならバリデーションが通らない' do
+    task = Task.new(content:"#{ "あ" * 51 }", detail: "失敗テスト")
+    expect(task).not_to be_valid
+  end
+  it 'detailが空でもcontentが入力されていればバリデーションが通る' do
+    task = Task.new(content:"成功テスト", detail: "")
+    expect(task).to be_valid
+  end
+  it 'detailが255文字以上ならバリデーションが通らない' do
+    task = Task.new(content:"失敗テスト", detail: "#{ "f" * 260 }")
+    expect(task).not_to be_valid
+  end
 end
