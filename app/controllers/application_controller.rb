487d983dfdc55fb8262c259c6e808f951a988d44
Haruka Teduka <th_kiyose@outlook.com>
2020-06-10 10:30:34 +0900

    BASIC認証を設定

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,12 @@
 class ApplicationController < ActionController::Base
-  http_basic_authenticate_with :name => ENV['BASIC_AUTH_USERNAME'], :password => ENV['BASIC_AUTH_PASSWORD'] if Rails.env == "production"
+  before_action :basic
+
+  private
+  def basic
+    if Rails.env == "production"
+      authenticate_or_request_with_http_basic do |name, password|
+        name == ENV['BASIC_AUTH_NAME'] && password == ENV['BASIC_AUTH_PASSWORD']
+      end
+    end
+  end
 end
