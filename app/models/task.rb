f07ab79effa083f0607f0c8486239321e303b4aa
Haruka Teduka <th_kiyose@outlook.com>
2020-06-11 16:41:43 +0900

    statusカラムを追加、enumを定義

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,4 +1,5 @@
 class Task < ApplicationRecord
+  enum status: { "未着手": 0, "進行中": 1, "完了": 2}
   validates :content, presence: true, length: { maximum: 50 }
   validates :detail, length: { maximum: 255 }
 end
---
+++ db/migrate/20200611072334_add_column_status_to_tasks.rb
@@ -0,0 +1,5 @@
+class AddColumnStatusToTasks < ActiveRecord::Migration[5.2]
+  def change
+    add_column :tasks, :status, :integer
+  end
+end
