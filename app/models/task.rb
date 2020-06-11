1e5851a8bdef7ee0ba61e76afe89f34f8df40625
Haruka Teduka <th_kiyose@outlook.com>
2020-06-11 16:45:22 +0900

    enum定義を変更

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,5 +1,5 @@
 class Task < ApplicationRecord
-  enum status: { "未着手": 0, "進行中": 1, "完了": 2}
+  enum status: [:waiting, :working, :complete]
   validates :content, presence: true, length: { maximum: 50 }
   validates :detail, length: { maximum: 255 }
 end
