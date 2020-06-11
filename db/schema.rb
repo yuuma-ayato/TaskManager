efe8a791f9bd9faca72ab7b1644a5d037510b4db
Haruka Teduka <th_kiyose@outlook.com>
2020-06-11 16:47:52 +0900

    statusのdb制約を追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_11_072334) do
+ActiveRecord::Schema.define(version: 2020_06_11_074707) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "tasks", force: :cascade do |t|
-    t.string "content", limit: 50, null: false
+    t.string "content", default: "0", null: false
     t.text "detail"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
