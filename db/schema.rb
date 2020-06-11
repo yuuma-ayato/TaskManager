5ef76990ce72b96e753e53d076ac04c8951b9434
Haruka Teduka <th_kiyose@outlook.com>
2020-06-11 16:54:50 +0900

    db制約にミスがあったので再設定

--- db/schema.rb
+++ db/schema.rb
@@ -10,18 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_11_074707) do
+ActiveRecord::Schema.define(version: 2020_06_11_075315) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "tasks", force: :cascade do |t|
-    t.string "content", default: "0", null: false
+    t.string "content", limit: 50, default: "0", null: false
     t.text "detail"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.datetime "limit"
-    t.integer "status"
+    t.integer "status", default: 0, null: false
   end
 
 end
