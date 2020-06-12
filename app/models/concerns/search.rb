eb4d6c08979389bde827e0e67f9741f40322e12b
Haruka Teduka <th_kiyose@outlook.com>
2020-06-12 13:50:58 +0900

    statusでの検索機能を実装

--- app/models/concerns/search.rb
+++ app/models/concerns/search.rb
@@ -5,5 +5,6 @@ module Search
 
   included do
     scope :what_content, -> (content) { where("content LIKE ?","%#{ content }%") if content.present?}
+    scope :what_status, -> (status) { where( status: status ) if status.present?}
   end
 end
