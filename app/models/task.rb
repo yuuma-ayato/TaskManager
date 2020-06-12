e880b9cd771a51a910365f16724e29a6f4e8ab74
Haruka Teduka <th_kiyose@outlook.com>
2020-06-12 16:42:28 +0900

    enumを定義

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,7 +1,8 @@
 class Task < ApplicationRecord
   include Search
   include Sort
-  enum status: [:未着手, :着手中, :完了]
+  enum status: %i(未着手 着手中 完了)
+  enum priority: %i(低 中　高)
   validates :content, presence: true, length: { maximum: 50 }
   validates :detail, length: { maximum: 255 }
 end
