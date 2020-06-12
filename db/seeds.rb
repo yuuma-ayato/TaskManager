8e0462f4ebd27487009c911f63d8716b3194ed27
Haruka Teduka <th_kiyose@outlook.com>
2020-06-12 16:52:01 +0900

    seedデータを修正

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,6 @@
 content_sample = ["テストをする","シードデータを作る","Railsを楽しむ"]
-every_status = [0,1,2]
+values = [0,1,2]
 
-10.times do |n|
-  Task.create(content:content_sample.sample + "#{n}", detail: "シードデータで投入したタスクです", limit: DateTime.now, status: every_status.sample)
+20.times do |n|
+  Task.create(content:content_sample.sample + "#{n}", detail: "シードデータで投入したタスクです", limit: DateTime.now, status: values.sample, priority:values.sample)
 end
