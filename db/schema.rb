990f6609cefb45217f14a90b6ddc8a6452cfcc34
Haruka Teduka <th_kiyose@outlook.com>
2020-06-15 11:38:33 +0900

    user_idをtaskに追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_15_023037) do
+ActiveRecord::Schema.define(version: 2020_06_15_023559) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,7 +23,9 @@ ActiveRecord::Schema.define(version: 2020_06_15_023037) do
     t.datetime "limit"
     t.integer "status", default: 0, null: false
     t.integer "priority", default: 1, null: false
+    t.bigint "user_id"
     t.index ["content"], name: "index_tasks_on_content"
+    t.index ["user_id"], name: "index_tasks_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -34,4 +36,5 @@ ActiveRecord::Schema.define(version: 2020_06_15_023037) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "tasks", "users"
 end
