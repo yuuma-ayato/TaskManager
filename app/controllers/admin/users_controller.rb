b400682b1b1ab36d04006b3e282cbd4fd67e5b5c
Haruka Teduka <th_kiyose@outlook.com>
2020-06-15 19:15:32 +0900

    管理画面からユーザー詳細確認・編集・削除を出来るよう実装

---
+++ app/controllers/admin/users_controller.rb
@@ -0,0 +1,50 @@
+class Admin::UsersController < ApplicationController
+  before_action :set_user, only: %i(show edit update destroy)
+  def index
+    @users = User.all
+  end
+
+  def new
+    @user = User.new
+  end
+
+  def show
+  end
+
+  def create
+    @user= User.new(user_params)
+    if @user.save
+      redirect_to admin_users_path
+      flash[:notice] = "ユーザーを作成しました"
+    else
+      render :new
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @user.update(user_params)
+      redirect_to admin_users_path
+      flash[:notice] = "ユーザーを編集しました"
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @user.destroy
+    redirect_to admin_users_path
+    flash[:notice] = "ユーザーを削除しました"
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:name,:email,:password,:password_confirmation)
+  end
+
+  def set_user
+    @user = User.find(params[:id])
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,7 @@ Rails.application.routes.draw do
     end
   end
   namespace :admin do
-    resources :users, only: %i(new create show)
+    resources :users, only: %i(index new create show edit update destroy)
   end
   resources :sessions, only: %i(new create destroy)
 end
