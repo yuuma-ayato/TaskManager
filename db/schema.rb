a9fdf577fb5bde0caf45928b7335de5d0ce5b1de
Haruka Teduka <th_kiyose@outlook.com>
2020-06-15 11:33:29 +0900

    Userモデルの作成,seedデータ投入

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_12_073619) do
+ActiveRecord::Schema.define(version: 2020_06_15_023037) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -24,6 +24,14 @@ ActiveRecord::Schema.define(version: 2020_06_12_073619) do
     t.integer "status", default: 0, null: false
     t.integer "priority", default: 1, null: false
     t.index ["content"], name: "index_tasks_on_content"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.string "password_digest"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -4,3 +4,7 @@ values = [0,1,2]
 20.times do |n|
   Task.create(content:content_sample.sample + "#{n}", detail: "シードデータで投入したタスクです", limit: DateTime.now, status: values.sample, priority:values.sample)
 end
+
+5.times do |n|
+  User.create(name:"user" + "#{n}", email: "test" + "#{n}" + "@test.com", password_digest: "password")
+end
