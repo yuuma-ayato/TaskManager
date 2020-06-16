cb2d418e75c384a33825f561360f71567ab5e74a
Haruka Teduka <th_kiyose@outlook.com>
2020-06-16 16:06:07 +0900

    seedデータにユーザー追加

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,13 @@
 content_sample = ["テストをする","シードデータを作る","Railsを楽しむ"]
 values = [0,1,2]
 
+user1 = User.create(name:"一般ユーザー",email:"general@test.com",password:"password",admin:false)
+user2 = User.create(name:"管理ユーザー",email:"admin@test.com",password:"password",admin:true)
+
+
 20.times do |n|
-  Task.create(content:content_sample.sample + "#{n}", detail: "シードデータで投入したタスクです", limit: DateTime.now, status: values.sample, priority:values.sample, user_id:30)
+  user1.tasks.build(content:content_sample.sample + "#{n}", detail: "シードデータで投入したタスクです", limit: DateTime.now, status: values.sample, priority:values.sample)
+end
+20.times do |n|
+  user2.tasks.build(content:content_sample.sample + "#{n}", detail: "シードデータで投入したタスクです", limit: DateTime.now, status: values.sample, priority:values.sample)
 end
