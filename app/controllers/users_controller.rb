906200237026d1ee7c9bf00bbc410e2f9ca2dd4c
Haruka Teduka <th_kiyose@outlook.com>
2020-06-16 13:48:32 +0900

    showの分岐を修正

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -25,7 +25,7 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
-    if @user.id == current_user.id
+    if @user.id != current_user.id
       redirect_to tasks_path
     end
   end
