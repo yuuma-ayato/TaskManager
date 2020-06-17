6d551c244503723a477984406a8e98e805ff5b06
Haruka Teduka <th_kiyose@outlook.com>
2020-06-17 11:15:03 +0900

    ラベル作成時colorをenumから選ぶように変更

--- app/models/label.rb
+++ app/models/label.rb
@@ -1,4 +1,6 @@
 class Label < ApplicationRecord
+  enum color: { black: "#000000", gray: "#808080", red: "#dc143c", blue: "#4169e1",
+                green: "#3cb371", orange: "#ff8c00", purple: "#ba55d3", brown: "#8b4513"}
   validates :label_name, presence: true, uniqueness: true
   validates :color, length: { maximum: 7 }
 end
