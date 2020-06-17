62bb3eb140fd87a1091f3e87896462da5d7725c6
Haruka Teduka <th_kiyose@outlook.com>
2020-06-17 11:38:10 +0900

    ラベルの編集・削除機能を実装

--- app/controllers/admin/labels_controller.rb
+++ app/controllers/admin/labels_controller.rb
@@ -1,6 +1,7 @@
 class Admin::LabelsController < ApplicationController
   include Common
   before_action :admin_only
+  before_action :set_label, only: %i(edit update destroy)
   def index
     @labels = Label.all
   end
@@ -19,7 +20,30 @@ class Admin::LabelsController < ApplicationController
     end
   end
 
+  def edit
+  end
+
+  def update
+    if @label.update(label_params)
+      redirect_to admin_labels_path
+      flash[:notice] = "ラベルを編集しました"
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @label.destroy
+    redirect_to admin_labels_path
+    flash[:notice] = "ラベルを削除しました"
+  end
+
   private
+
+  def set_label
+    @label = Label.find(params[:id])
+  end
+
   def label_params
     params.require(:label).permit(:label_name,:color)
   end
